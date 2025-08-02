5390d8e1595c7d4308a146564ed7c65bb7c95c4a
Binarybraniac01 <binarybraniac01@gmail.com>
2025-08-02 16:57:11 +0530

    .

--- core/settings.py
+++ core/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR , 'public/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/media') 
