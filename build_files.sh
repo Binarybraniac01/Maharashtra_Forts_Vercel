f2551f09c3bfc72206a67ec823146ed98ff25a2e
Amey Ambavkar <binarybraniac01@gmail.com>
2026-01-30 00:26:38 +0530

    Update build_files.sh to include break-system-packages
    Added option to bypass PEP 668 error during pip install.

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 echo "BUILD START"
-python3 -m pip install -r requirements.txt
+
+# Add --break-system-packages to bypass the PEP 668 error
+python3 -m pip install -r requirements.txt --break-system-packages
+
 python3 manage.py collectstatic --noinput --clear
 echo "BUILD END"
